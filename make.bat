913b08c2fa2bc49ad3fc753762217eeca9fcd6b2
lengors <pedrocavadinhas@gmail.com>
2018-12-24 01:05:23 +0000

    Update

--- make.bat
+++ make.bat
@@ -1,8 +1,25 @@
 @echo off
+set ARGC=0
+for %%x in (%*) do set /A ARGC+=1
+if %ARGC% equ 1 (
+	set JAVAC_PATH=%1
+) else (
+	set JAVAC_PATH="C:\Program Files\Java\jdk1.8.0_172\bin"
+)
+echo %PATH% | find /C /I %JAVAC_PATH% > nul
+if %errorlevel% equ 0 goto main
+if not exist %JAVAC_PATH% goto end_error
+set PATH=%PATH%;%JAVAC_PATH%
+
+:main
 dir /s /b src\*.java > sources
-echo %PATH% | find /C /I "Java\jdk1.8" > nul
-if %errorlevel% equ 1 set PATH=%PATH%;"C:\Program Files\Java\jdk1.8.0_172\bin"
 if exist bin rmdir /S /Q bin
 mkdir bin
 javac -d bin @sources
 del sources
+goto end
+
+:end_error
+echo "Invalid file path! Please provide a valid one"
+
+:end
